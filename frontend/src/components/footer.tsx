575d7aaead871680784d355326000086729430db
Theycallmeki <142416427+Theycallmeki@users.noreply.github.com>
2025-06-19 12:32:23 +0800

    Update footer.tsx

--- frontend/src/components/footer.tsx
+++ frontend/src/components/footer.tsx
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <footer style={{ padding: "1rem", backgroundColor: "#eee", marginTop: "2rem" }}>
-      <p>© 2025 My Simple Next.js App.</p>
+      <p>© 2025 My Simple tomboy maglaqui.</p>
     </footer>
   );
 }
